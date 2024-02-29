c22f5d4c5722f0ab9c493d3d08ee462bd1cffa59
Evgeny Kuptsov <kupzov200@mail.ru>
2024-02-29 15:33:21 +0400

    refactor: switch to typescript in the controller file

--- src/components/controller/controller.ts
+++ src/components/controller/controller.ts
@@ -1,7 +1,11 @@
 import AppLoader from './appLoader';
 
 class AppController extends AppLoader {
-    getSources(callback) {
+    // ЕСЛИ ОШИБКИ, ТО ПОМЕНЯЙ callback типизацию
+    getSources(callback: void): void {
+        if (callback === undefined) throw new Error('Error. ');
+        console.log(callback);
+
         super.getResp(
             {
                 endpoint: 'sources',
@@ -10,20 +14,22 @@ class AppController extends AppLoader {
         );
     }
 
-    getNews(e, callback) {
-        let target = e.target;
-        const newsContainer = e.currentTarget;
+    getNews(e: Event, callback: void) {
+        if (callback === undefined) throw new Error('Error. ');
+
+        let target = e.target as HTMLElement;
+        const newsContainer = e.currentTarget as HTMLElement;
 
         while (target !== newsContainer) {
             if (target.classList.contains('source__item')) {
                 const sourceId = target.getAttribute('data-source-id');
                 if (newsContainer.getAttribute('data-source') !== sourceId) {
-                    newsContainer.setAttribute('data-source', sourceId);
+                    newsContainer.setAttribute('data-source', sourceId || '');
                     super.getResp(
                         {
                             endpoint: 'everything',
                             options: {
-                                sources: sourceId,
+                                sources: sourceId || '',
                             },
                         },
                         callback
@@ -31,7 +37,7 @@ class AppController extends AppLoader {
                 }
                 return;
             }
-            target = target.parentNode;
+            target = target.parentNode as HTMLElement;
         }
     }
 }
--- src/components/controller/loader.ts
+++ src/components/controller/loader.ts
@@ -24,7 +24,7 @@ class Loader {
             console.error('No callback for GET response');
         }
     ) {
-        console.log(endpoint);
+        // console.log(endpoint);
         this.load('GET', endpoint, callback, options);
     }
 
